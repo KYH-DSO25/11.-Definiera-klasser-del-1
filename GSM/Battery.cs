5b0e35fd7dd650e81c5317d800fb5aeb2b4cdbc0
Claes Engelin <claes.engelin@zocom.se>
2025-10-15 09:20:10 +0200

    Skapat Display och Battery-klasser

--- GSM/Battery.cs
+++ GSM/Battery.cs
@@ -1,13 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace GSM;
-internal class Battery
+public class Battery
 {
     // Privata konstanter
     private const uint MaxIdleHours = 500;
     private const uint MaxTalkHours = 3000;
+
+    // Privata fält
+    private uint? hoursIdle = null;
+    private uint? hoursTalk = null;
+
+    // Konstruktorer
+    public Battery(Type model, uint? hoursIdle = null, uint? hoursTalk = null)
+    {
+        this.Model = model;
+        this.hoursIdle = hoursIdle;
+        this.hoursTalk = hoursTalk;
+    }
+
+    // Enumeration
+    public enum Type
+    {
+        LiIon,
+        NiMH,
+        NiCd
+    }
+
+    // Egenskaper
+    public Type Model { get; set; }
+
+    public uint? HoursIdle
+    {
+        get {  return hoursIdle; }
+        set
+        {
+            if (value.GetValueOrDefault() > MaxIdleHours)
+            {
+                throw new ArgumentOutOfRangeException("Max inaktiv tid är för stor!");
+            }
+            else
+            {
+                this.hoursIdle = value;
+            }
+        }
+    }
+    public uint? HoursTalk
+    {
+        get { return hoursTalk; }
+        set
+        {
+            if (value.GetValueOrDefault() > MaxTalkHours)
+            {
+                throw new ArgumentOutOfRangeException("Max taltid är för stort!");
+            }
+            else
+            {
+                hoursTalk = value;
+            }
+        }
+    }
+
+    // Override metod
+    public override string ToString()
+    {
+        StringBuilder batteryInfo = new StringBuilder();
+
+        batteryInfo.AppendLine("--> BATTERI <--");
+        batteryInfo.AppendLine("Modell: " + Model.ToString());
+
+        if (hoursIdle.HasValue)
+        {
+            batteryInfo.AppendLine("Inaktiv tid: " + hoursIdle + " timmar");
+        }
+        if (hoursTalk.HasValue)
+        {
+            batteryInfo.AppendLine("Taltid: " + hoursTalk + " timmar");
+        }
+
+        return batteryInfo.ToString();
+    }
 }
---
+++ GSM/Display.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSM;
+public class Display
+{
+    // Privata konstanter
+    private const decimal MinSizeInches = 0.5m;
+    private const decimal MaxSizeInches = 100m;
+    private const decimal MaxNumberOfColors = 1E10m;
+
+    // Privata fält
+    private decimal? sizeInches = null;
+    private ulong? numberOfColors = null;
+
+    // Konstruktorer
+    public Display(decimal? sizeInches = null, ulong? numberOfColors = null)
+    {
+        SizeInches = sizeInches;
+        NumberOfColors = numberOfColors;
+    }
+
+    // Egenskaper
+    public decimal? SizeInches
+    {
+        get { return sizeInches; }
+        set
+        {
+            if (value.HasValue && (value < MinSizeInches || value > MaxSizeInches))
+            {
+                throw new ArgumentOutOfRangeException("Otillåten skärmstorlek!")
+            }
+            sizeInches = value;
+        }
+    }
+    public ulong? NumberOfColors
+    {
+        get { return this.numberOfColors; }
+        set
+        {
+            if (value.HasValue && value > MaxNumberOfColors)
+                throw new ArgumentOutOfRangeException("Antalet färger är för stort!");
+
+            this.numberOfColors = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        StringBuilder displayInfo = new StringBuilder();
+
+        displayInfo.AppendLine("--> SKÄRM <--");
+
+        if (sizeInches.HasValue)
+        {
+            displayInfo.AppendLine(string.Format("Storlek: {0} tum", sizeInches));
+        }
+        if (numberOfColors.HasValue)
+        {
+            displayInfo.AppendLine("Antal färger: " + numberOfColors);
+        }
+
+        return displayInfo.ToString();
+    }
+}
