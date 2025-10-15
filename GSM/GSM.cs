856b469618f93a01a2c0286e57089d6315abcf0b
Claes Engelin <claes.engelin@zocom.se>
2025-10-15 09:56:38 +0200

    Efter uppgift 5

---
+++ GSM/GSM.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSM;
+public class GSM
+{
+    // Privata konstanter
+    private const uint MaxPriceValue = 1_000_000;
+
+    // Private Fields
+    private string? model = null;
+    private string? manufacturer = null;
+    private string? owner = null;
+    private decimal? price = 0;
+
+    private Display display = null;
+    private Battery battery = null;
+
+    // Konstruktorer
+    public GSM(string model, string manufacturer, string owner = null,
+        decimal? price = null, Display display = null, Battery battery = null)
+    {
+        Model = model;
+        Manufacturer = manufacturer;
+        Owner = owner;
+        Price = price;
+        Display = display;
+        Battery = battery;
+    }
+
+    // Egenskaper
+    public string Model
+    {
+        get { return model; }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException("Modellell måste anges!");
+            }
+            model = value;
+        }
+    }
+    public string Manufacturer
+    {
+        get { return manufacturer; }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException("Tillverkare måste anges!");
+            }
+            manufacturer = value;
+        }
+    }
+
+    public string Owner
+    {
+        get { return owner; }
+        set { owner = value; }
+    }
+
+    public decimal? Price
+    {
+        get { return price; }
+        set
+        {
+            if (value.HasValue && (value < 0 || value > MaxPriceValue))
+            {
+                throw new ArgumentOutOfRangeException("Priset är för högt eller mindre än noll!");
+            }
+            price = value;
+        }
+    }
+
+    public Display Display
+    {
+        get { return display; }
+        set { display = value; }
+    }
+
+    public Battery Battery
+    {
+        get { return battery; }
+        set { battery = value; }
+    }
+
+    // Metoder
+    public void ShowInformation()
+    {
+        Console.Write(this.ToString());
+    }
+
+    public override string ToString()
+    {
+        StringBuilder mobileInfo = new StringBuilder();
+
+        mobileInfo.AppendLine("--------- MOBILTELEFON ----------");
+        mobileInfo.AppendLine("Modell: " + model);
+        mobileInfo.AppendLine("Tillverkare: " + manufacturer);
+
+        if (owner != null)
+        {
+            mobileInfo.AppendLine("Ägare: " + owner);
+        }
+
+        if (price.HasValue)
+        {
+            mobileInfo.AppendLine(string.Format("Pris: {0} kr", price));
+        }
+
+        if (display != null)
+        {
+            mobileInfo.AppendLine( Environment.NewLine + display.ToString());
+        }
+
+        if (battery != null)
+        {
+            mobileInfo.AppendLine( Environment.NewLine + battery.ToString());
+        }
+
+        return mobileInfo.ToString();
+    }
+}
